32e71c4c4d4acd0565f09e68597c59dd90ebfe73
DESKTOP-GGT7S4I\김시은 <se.kim@emih.co.kr>
2018-10-11 17:37:55 +0900

    no message

---
+++ src/main/java/com/dante/main/DanteApplication.java
@@ -0,0 +1,12 @@
+package com.dante.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dante/main/DanteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dante.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DanteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
