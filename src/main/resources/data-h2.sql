d85af9c2e2d6b3a520d644003cd90dbf86e75a07
DESKTOP-GGT7S4I\김시은 <se.kim@emih.co.kr>
2018-11-02 15:09:50 +0900

    20181102-2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,2 @@
-﻿insert into posts (title, author, content, created_date, modified_date) values ('테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
+﻿insert into posts (title, author, content, created_date, modified_date) values ('test1', 'test1@gmail.com', 'test1_content', now(), now());
+insert into posts (title, author, content, created_date, modified_date) values ('test2', 'test2@gmail.com', 'test2_content', now(), now());
